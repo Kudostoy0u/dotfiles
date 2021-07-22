d13e5e120da2cd99a0a9632cc2f151fd55f92dcb
Kudos Beluga <69732000+Kudostoy0u@users.noreply.github.com>
2021-07-22 13:04:07 -0500

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,224 +1,120 @@
-﻿set fish_greeting
-set VIRTUAL_ENV_DISABLE_PROMPT "1"
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+# No introductory sentence
+set -g -x fish_greeting ''
 
-# Set settings for https://github.com/franciscolourenco/done
-set -U __done_min_cmd_duration 10000
-set -U __done_notification_urgency_level low
+# Quick way to access fish config
+alias fishrc='nvim ~/.config/fish/config.fish'
 
+# Quick way to access vim config
+alias vimrc='nvim ~/.vimrc'
 
-## Environment setup
-# Apply .profile
-source ~/.profile
-
-# Add ~/.local/bin to PATH
-if test -d ~/.local/bin
-    if not contains -- ~/.local/bin $PATH
-        set -p PATH ~/.local/bin
-    end
-end
-
-# Add depot_tools to PATH
-if test -d ~/Applications/depot_tools
-    if not contains -- ~/Applications/depot_tools $PATH
-        set -p PATH ~/Applications/depot_tools
-    end
-end
-
-
-## Starship prompt
-if status --is-interactive
-   source ("/usr/bin/starship" init fish --print-full-init | psub)
-end
-
-
-## Functions
-# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
-function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
-end
-
-function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
-end
-
-if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
-else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
-end
-
-# Fish command history
-function history
-    builtin history --show-time='%F %T '
-end
-
-function backup --argument filename
-    cp $filename $filename.bak
-end
-
-# Copy DIR1 DIR2
-function copy
-    set count (count $argv | tr -d \n)
-    if test "$count" = 2; and test -d "$argv[1]"
-	set from (echo $argv[1] | trim-right /)
-	set to (echo $argv[2])
-        command cp -r $from $to
-    else
-        command cp $argv
-    end
-end
-
-
-## Useful aliases
-# Replace ls with exa
-alias ls='exa --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons'  # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
-alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
-
-# Replace some more things with better alternatives
-# alias cat='bat --style header --style rules --style snip --style changes --style header'
-[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru --bottomup'
-
-# Common use
-alias aup="pamac upgrade --aur"
-alias grubup="sudo update-grub"
-alias fixpacman="sudo rm /var/lib/pacman/db.lck"
-alias tarnow='tar -acf '
-alias untar='tar -zxvf '
-alias wget='wget -c '
-alias rmpkg="sudo pacman -Rdd"
-alias psmem='ps auxf | sort -nr -k 4'
-alias psmem10='ps auxf | sort -nr -k 4 | head -10'
-alias upd='sudo reflector --latest 5 --age 2 --fastest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist && cat /etc/pacman.d/mirrorlist && sudo pacman -Syu && fish_update_completions && sudo updatedb && sudo -H DIFFPROG=meld pacdiff'
+# Quick way to cd back n directories
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias hw='hwinfo --short'                                   # Hardware Info
-alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB (expac must be installed)
-alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'			# List amount of -git packages
 
-# Get fastest mirrors 
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist" 
-alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist" 
-alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist" 
-alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist" 
+# Shorthand of zoxide
+alias z="zoxide"
 
-# Help people new to Arch
-alias apt='man pacman'
-alias apt-get='man pacman'
-alias pacdiff='sudo -H DIFFPROG=meld pacdiff'               # Compare .pacnew & .pacsave files 
-alias helpme='cht.sh --shell'
-alias please='sudo'
-alias tb='nc termbin.com 9999'
-alias paru="paru --bottomup"
+# Initialize zoxide
+z init fish | source
 
-# Cleanup orphaned packages
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-
-# Get the error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
-
-# Recent installed packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
-
-
-## Import colorscheme from 'wal' asynchronously
-if type "wal" >> /dev/null 2>&1
-   cat ~/.cache/wal/sequences
-end
-
-# Add snap support
-fish_add_path /var/lib/snapd/snap/bin
-
-# Add cargo (from Rust) to path
-set PATH $HOME/.cargo/bin $PATH
-
-# Quickly edit fish config
-alias fishrc="nvim ~/.config/fish/config.fish"
-
-# Because typing sudo takes too long
-alias pacman="sudo pacman"
-
-# Because typing an "n" before vim is too time consuming
+# Because sometimes I forget to type the "n"
 alias vim="nvim"
 
-# Use autojump for quick navigation across the filesystem
-if test -f /home/kudos/.autojump/share/autojump/autojump.fish; . /home/kudos/.autojump/share/autojump/autojump.fish; end
-. /usr/share/autojump/autojump.fish
+# Add directories to path
+fish_add_path /home/kudos/.cargo/bin
+fish_add_path /home/kudos/.local/bin
 
-# Run Haskell executables from anywhere
-set PATH /home/kudos/.local/bin $PATH
-
-# Binaries
-set path /usr/local $PATH
-
-# Get all files - including hidden ones - in a directory and show their sizes
-alias sizes="du -sh -- * .*"
-
-# When screenshots accumulate, quick way to delete them all
-alias delete="rm ~/Pictures/Screenshot_*"
-
-# New tab in Konsole
-alias newtab="qdbus $KONSOLE_DBUS_SERVICE $KONSOLE_DBUS_WINDOW newSession"
-
-# Quick way to access vimrc
-alias vimrc="vim ~/.vimrc"
-
-# Because typing an extra "l" takes too long
-alias l="ls"
-
-# Quicker than using cd
-function j 
- cd (autojump $argv)
-end
-
-# Docker doesn't start by default, and I don't want to have it running because most of the time I don't use it
-function docker
-  if [ "$argv" = "begin" ]
-    systemctl start docker
-  else if [ "$argv" = "end" ]
-    systemctl stop docker
-  else
-    /usr/bin/docker $argv
-  end
-end
-
-# Exit the terminal
-alias exit="exec true"
-
-# Because if it doesn't work, sudo will make it
-alias please="sudo"
-
-# Fancier way to display files
-alias cat="bat"
-
-# When I need to display files for copy/paste
+# When I want to use the real cat
 alias meow="/usr/bin/cat"
 
-# Kill node.js process running on 8080
-function 8080
-  kill (lsof -i :8080 | awk '{print $2}' | sed -n '2p')
+# Because bat defaults to batcat for some reason
+alias bat="batcat"
+
+# Use bat instead of cat
+alias cat="bat"
+
+# When will I ever use apt without sudo?
+alias apt="sudo apt"
+
+# Quick way to install packages
+function get
+  apt install $argv[1]
 end
+
+# Quick way to update
+alias update="apt update && apt upgrade -y"
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+
+# Use double bang in fish to execute previous commands
+
+function bind_bang
+    switch (commandline --current-token)[-1]
+    case "!"
+        # Without the `--`, the functionality can break when completing
+        # flags used in the history (since, in certain edge cases
+        # `commandline` will assume that *it* should try to interpret
+        # the flag)
+        commandline --current-token -- $history[1]
+        commandline --function repaint
+    case "*"
+        commandline --insert !
+    end
+end
+
+function bind_dollar
+    switch (commandline --current-token)[-1]
+    # This case lets us still type a literal `!$` if we need to (by
+    # typing `!\$`). Probably overkill.
+    case "*!\\"
+        # Without the `--`, the functionality can break when completing
+        # flags used in the history (since, in certain edge cases
+        # `commandline` will assume that *it* should try to interpret
+        # the flag)
+        commandline --current-token -- (echo -ns (commandline --current-token)[-1] | head -c '-1')
+        commandline --insert '$'
+    case "!"
+        commandline --current-token ""
+        commandline --function history-token-search-backward
+
+
+    # Main difference from referenced version is this `*!` case
+    # =========================================================
+    #
+    # If the `!$` is preceded by any text, search backward for tokens
+    # that contain that text as a substring. E.g., if we'd previously
+    # run
+    #
+    #   git checkout -b a_feature_branch
+    #   git checkout master
+    #
+    # then the `fea!$` in the following would be replaced with
+    # `a_feature_branch`
+    #
+    #   git branch -d fea!$
+    #
+    # and our command line would look like
+    #
+    #   git branch -d a_feature_branch
+    #
+    case "*!"
+        # Without the `--`, the functionality can break when completing
+        # flags used in the history (since, in certain edge cases
+        # `commandline` will assume that *it* should try to interpret
+        # the flag)
+        commandline --current-token -- (echo -ns (commandline --current-token)[-1] | head -c '-1')
+        commandline --function history-token-search-backward
+    case "*"
+        commandline --insert '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; test -f /home/kudos/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /home/kudos/.ghcup/bin $PATH # ghcup-env
